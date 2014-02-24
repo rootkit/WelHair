0a7f07b578b168fc104413cfc1b4879edbc21500
larrylu <larry@welfony.com>
2014-02-24 23:36:20 +0800

    add

---
+++ src/ios/App/Controllers/Hairstylists/HairStylistsViewController.h
@@ -0,0 +1,13 @@
+//
+//  HairStylistsViewController.h
+//  WelHair
+//
+//  Created by lu larry on 2/24/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HairStylistsViewController : BaseViewController
+
+@end
