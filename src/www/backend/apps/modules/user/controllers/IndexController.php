0c29c7d0ea49e7a8d86dd1c17ba04c9212952807
Edison <yongjie@welfony.cm>
2014-02-26 00:10:20 +0800

    Add jQuery UI

--- src/www/backend/apps/modules/user/controllers/IndexController.php
+++ src/www/backend/apps/modules/user/controllers/IndexController.php
@@ -11,6 +11,16 @@ class User_IndexController extends Zend_Controller_Action
     public function infoAction()
     {
         $this->view->pageTitle = '会员信息';
+
+        if ($this->_request->isPost()) {
+            $userrole = intval($this->_request->getParam('userrole'));
+            $username = htmlspecialchars($this->_request->getParam('username'));
+            $nickname = htmlspecialchars($this->_request->getParam('nickname'));
+            $email = htmlspecialchars($this->_request->getParam('email'));
+            $mobile = htmlspecialchars($this->_request->getParam('mobile'));
+
+            echo $email;die();
+        }
     }
 
 }
