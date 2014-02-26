ac0b562f5ac5d51ad28c681d906e7be2811b48e5
larrylu <larry@welfony.com>
2014-02-26 23:07:35 +0800

    baidu map

---
+++ src/ios/App/Controllers/Common/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  WelHair
+//
+//  Created by lu larry on 2/26/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
