934872d862f336df5b510c1ebde6933db683183e
larrylu <larry@welfony.com>
2014-03-05 23:25:25 +0800

    base flow

---
+++ src/ios/App/Controllers/Order/PayViewController.m
@@ -0,0 +1,38 @@
+//
+//  PayViewController.m
+//  WelHair
+//
+//  Created by lu larry on 3/5/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "PayViewController.h"
+
+@interface PayViewController ()
+
+@end
+
+@implementation PayViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
