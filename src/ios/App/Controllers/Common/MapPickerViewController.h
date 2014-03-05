c51f03cbbe76bf6bcb7e8c37af1fab46becca5ca
larrylu <larry@welfony.com>
2014-03-05 21:55:44 +0800

    pick address in map

---
+++ src/ios/App/Controllers/Common/MapPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapPickerViewController.h
+//  WelHair
+//
+//  Created by lu larry on 3/5/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface MapPickerViewController : BaseViewController
+
+@end
---
+++ src/ios/App/Vendor/WelKit/UIKit/UITextField+WelKit.m
@@ -0,0 +1,13 @@
+//
+//  UITextField+WelKit.m
+//  WelHair
+//
+//  Created by lu larry on 3/5/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "UITextField+WelKit.h"
+
+@implementation UITextField (WelKit)
+
+@end
