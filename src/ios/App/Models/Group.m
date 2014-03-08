6603753c5e4cfd2d53dad5fdbf5ccac2dbbd986b
larrylu <larry@welfony.com>
2014-03-08 16:52:35 +0800

    group

---
+++ src/ios/App/Models/Group.m
@@ -0,0 +1,13 @@
+//
+//  Group.m
+//  WelHair
+//
+//  Created by lu larry on 3/8/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "Group.h"
+
+@implementation Group
+
+@end
---
+++ src/ios/App/Views/GroupCell.m
@@ -0,0 +1,29 @@
+//
+//  GroupCell.m
+//  WelHair
+//
+//  Created by lu larry on 3/8/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "GroupCell.h"
+
+@implementation GroupCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
