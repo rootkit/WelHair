59a1579c6c4abccea2acca6bfe0eb1f140dfc0e6
larrylu <larry@welfony.com>
2014-03-09 19:43:00 +0800

    dropdown

---
+++ src/ios/App/Views/DropDownView.h
@@ -0,0 +1,13 @@
+//
+//  DropDownView.h
+//  WelHair
+//
+//  Created by lu larry on 3/9/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DropDownView : UIView
+
+@end
---
+++ src/ios/App/Views/DropDownView.m
@@ -0,0 +1,31 @@
+//
+//  DropDownView.m
+//  WelHair
+//
+//  Created by lu larry on 3/9/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "DropDownView.h"
+
+@implementation DropDownView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
