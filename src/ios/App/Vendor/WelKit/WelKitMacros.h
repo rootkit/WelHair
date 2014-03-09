f3e3c1b3c0a29e0dfa20b5f783151c79f13c2f38
larrylu <larry@welfony.com>
2014-03-09 17:48:03 +0800

    add macro

--- src/ios/App/Vendor/WelKit/WelKitMacros.h
+++ src/ios/App/Vendor/WelKit/WelKitMacros.h
@@ -10,6 +10,25 @@
 //
 // ==============================================================================
 
+#ifdef DEBUG
+#define debugLog(...) NSLog(__VA_ARGS__)
+#define debugMethod() NSLog(@"%s", __func__)
+#else
+#define debugLog(...)
+#define debugMethod()
+#endif
+
+#define EMPTY_STRING        @""
+
+#define STR(key)            NSLocalizedString(key, nil)
+
+#define PATH_OF_APP_HOME    NSHomeDirectory()
+#define PATH_OF_TEMP        NSTemporaryDirectory()
+#define PATH_OF_DOCUMENT    [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0]
+
+#define APP_VERSION         [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"]
+
+
 #ifndef Macros_h
 #define Macros_h
 
