506bd07431d98941885db2e4c68d7df3894553f2
larrylu <larry@welfony.com>
2014-03-09 20:18:57 +0800

    comment

---
+++ src/ios/App/Views/CommentCell.m
@@ -0,0 +1,31 @@
+//
+//  CommentCell.m
+//  WelHair
+//
+//  Created by lu larry on 3/9/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "CommentCell.h"
+
+@implementation CommentCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
