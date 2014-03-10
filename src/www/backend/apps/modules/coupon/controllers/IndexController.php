a9aac6214960ab0f47c46ea565b74e164f99d242
winnoryu <winnoryu@gmail.com>
2014-03-10 21:14:41 +0800

    coupon related change.

---
+++ src/www/backend/apps/modules/coupon/controllers/IndexController.php
@@ -0,0 +1,30 @@
+<?php
+
+// ==============================================================================
+//
+// This file is part of the WelStory.
+//
+// Create by Welfony Support <support@welfony.com>
+// Copyright (c) 2012-2014 welfony.com
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+//
+// ==============================================================================
+
+use Welfony\Controller\Base\AbstractAdminController;
+
+class Coupon_IndexController extends AbstractAdminController
+{
+
+    public function searchAction()
+    {
+        $this->view->pageTitle = '优惠券列表';
+    }
+
+    public function infoAction()
+    {
+    	$this->view->pageTitle = '添加优惠券';
+    }
+
+}
