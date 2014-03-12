e63f0e7a66717bb0d648c1f27feb639f09111bbc
larrylu <larry@welfony.com>
2014-03-12 21:02:37 +0800

    staff detail

---
+++ src/ios/App/Views/StaffCell.h
@@ -0,0 +1,13 @@
+//
+//  StaffCell.h
+//  WelHair
+//
+//  Created by lu larry on 3/12/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StaffCell : UITableViewCell
+
+@end
