cccbb5f106a80a5e60d27e4633507362f8e2441b
larrylu <larry@welfony.com>
2014-03-16 14:21:39 +0800

    create coupon

---
+++ src/ios/App/Controllers/User/Group/GroupManage/Coupon/CreateCouponViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateCouponViewController.h
+//  WelHair
+//
+//  Created by lu larry on 3/16/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface CreateCouponViewController : BaseViewController
+
+@end
