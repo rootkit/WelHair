1c05a273d19c955d3066548a2afcb8207d7f36e1
larrylu <larry@welfony.com>
2014-03-16 13:45:35 +0800

    添加服务

---
+++ src/ios/App/Controllers/User/Staff/Service/StaffAddServiceViewController.h
@@ -0,0 +1,13 @@
+//
+//  StaffAddServiceViewController.h
+//  WelHair
+//
+//  Created by lu larry on 3/16/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface StaffAddServiceViewController : BaseViewController
+
+@end
