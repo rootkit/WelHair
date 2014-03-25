49e84d43549ec6cd4804a73d02e6fa5cca62ee82
larrylu <larry@welfony.com>
2014-03-25 22:27:19 +0800

    product flow

---
+++ src/ios/App/Controllers/Order/OrderPreviewViewController.h
@@ -0,0 +1,13 @@
+//
+//  OrderPreviewViewController.h
+//  WelHair
+//
+//  Created by lu larry on 3/25/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface OrderPreviewViewController : BaseViewController
+
+@end
---
+++ src/ios/App/Controllers/Products/ProductDetailWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProductDetailWebViewController.h
+//  WelHair
+//
+//  Created by lu larry on 3/25/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ProductDetailWebViewController : BaseViewController
+
+@end
