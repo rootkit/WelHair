c4977bb225b97143cfb82ce0ef38d935f2562c84
winnoryu <winnoryu@gmail.com>
2014-03-30 15:16:54 +0800

    商品规格UI

---
+++ src/www/static/js/backend/spec-info.js
@@ -0,0 +1,66 @@
+// ==============================================================================
+//
+// This file is part of the WelStory.
+//
+// Create by Welfony Support <support@welfony.com>
+// Copyright (c) 2012-2014 welfony.com
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+//
+// ==============================================================================
+
+$(function() {
+    $('#frm-model-info').Validform({
+        tiptype: 3
+    });
+
+    $('#btnAddSpec').click(function(){
+
+      var row = '   <tr> ' +
+                '               <td><input name="specval" type="text" value="" datatype="s" class="u-ipt"/></td> ' +
+                '               <td></td>' +
+                '            </tr>';
+
+        $('#spectable tbody').append( $(row));
+        $('.content').height($('.content').height() + 50);
+
+    });
+
+
+    $( "#frm-model-info" ).submit(function( event ) {
+
+
+          event.preventDefault();
+
+          var form = $( this );
+
+          var name = $('#modelname').val();
+
+          //var brandcategories  = $('input[name="category[]"]:checked').map(function(i,n) {
+          //      return $(n).val();
+          //}).get();
+         
+          
+
+          url = form.attr( "action" );
+
+          var posting = $.post( url, { 
+                'name': name
+            } );
+
+
+          posting.done(function( data ) {
+
+              window.location = globalSetting.baseUrl + '/goods/model/search';
+              return;
+
+          });
+
+      });
+
+    $('#btnCancel').click(function(){
+        window.location = globalSetting.baseUrl + '/goods/model/search';
+        return;
+    });
+});
