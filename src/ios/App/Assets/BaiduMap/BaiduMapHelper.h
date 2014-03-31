bb0a1f57041f852b257fa4d6cdd4b9da422b0b03
larrylu <larry@welfony.com>
2014-03-31 21:12:59 +0800

    top left city name

---
+++ src/ios/App/Assets/BaiduMap/BaiduMapHelper.h
@@ -0,0 +1,13 @@
+//
+//  BaiduMapHelper.h
+//  WelHair
+//
+//  Created by lu larry on 3/31/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaiduMapHelper : NSObject
+
+@end
---
+++ src/ios/App/Assets/BaiduMap/BaiduMapHelper.m
@@ -0,0 +1,13 @@
+//
+//  BaiduMapHelper.m
+//  WelHair
+//
+//  Created by lu larry on 3/31/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "BaiduMapHelper.h"
+
+@implementation BaiduMapHelper
+
+@end
