cd34718a3055325c9ab25d939dadb82acc5bbd9a
larrylu <larry@welfony.com>
2014-04-01 16:18:51 +0800

    group detail

---
+++ src/ios/App/Controllers/Groups/GroupStaffListViewController.h
@@ -0,0 +1,13 @@
+//
+//  GroupStaffListViewController.h
+//  WelHair
+//
+//  Created by lu larry on 4/1/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupStaffListViewController : UIViewController
+
+@end
