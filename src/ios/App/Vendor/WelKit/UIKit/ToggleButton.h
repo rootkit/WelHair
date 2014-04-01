2283833253d130628c9e97a5f6c915cfa2a232a1
larrylu <larry@welfony.com>
2014-04-01 14:57:29 +0800

    toggle button

---
+++ src/ios/App/Vendor/WelKit/UIKit/ToggleButton.h
@@ -0,0 +1,13 @@
+//
+//  ToggleButton.h
+//  WelHair
+//
+//  Created by lu larry on 4/1/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToggleButton : UIButton
+
+@end
