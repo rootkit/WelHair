362de56223434452850b347e1432db372b3b7d05
larrylu <larry@welfony.com>
2014-04-04 17:23:56 +0800

    staff detail

---
+++ src/ios/App/Models/Service.h
@@ -0,0 +1,13 @@
+//
+//  Service.h
+//  WelHair
+//
+//  Created by lu larry on 4/4/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "BaseModel.h"
+
+@interface Service : BaseModel
+
+@end
---
+++ src/ios/App/Models/Service.m
@@ -0,0 +1,13 @@
+//
+//  Service.m
+//  WelHair
+//
+//  Created by lu larry on 4/4/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "Service.h"
+
+@implementation Service
+
+@end
