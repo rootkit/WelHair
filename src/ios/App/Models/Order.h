36c491120300724536b6de306ee71318159e7095
larrylu <larry@welfony.com>
2014-04-07 13:04:42 +0800

    order preview

---
+++ src/ios/App/Models/Order.h
@@ -0,0 +1,13 @@
+//
+//  Order.h
+//  WelHair
+//
+//  Created by lu larry on 4/7/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "BaseModel.h"
+
+@interface Order : BaseModel
+
+@end
---
+++ src/ios/App/Models/Order.m
@@ -0,0 +1,13 @@
+//
+//  Order.m
+//  WelHair
+//
+//  Created by lu larry on 4/7/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "Order.h"
+
+@implementation Order
+
+@end
