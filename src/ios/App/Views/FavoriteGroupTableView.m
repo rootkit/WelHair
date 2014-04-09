52b6a24b63b818b0288db60239c20f3d8c1d91fa
larrylu <larry@welfony.com>
2014-04-09 17:30:07 +0800

    fav

---
+++ src/ios/App/Views/FavoriteGroupTableView.m
@@ -0,0 +1,31 @@
+//
+//  FavoriteGroupTableView.m
+//  WelHair
+//
+//  Created by lu larry on 4/9/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "FavoriteGroupTableView.h"
+
+@implementation FavoriteGroupTableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ src/ios/App/Views/FavoriteWorkTableView.m
@@ -0,0 +1,31 @@
+//
+//  FavoriteWorkTableView.m
+//  WelHair
+//
+//  Created by lu larry on 4/9/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "FavoriteWorkTableView.h"
+
+@implementation FavoriteWorkTableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
