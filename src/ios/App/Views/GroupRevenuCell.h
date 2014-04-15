f634a1e43bf65b6b7bfa546f550bb33b9b096711
larrylu <larry@welfony.com>
2014-04-15 22:31:21 +0800

    group revenu

---
+++ src/ios/App/Views/GroupRevenuCell.h
@@ -0,0 +1,13 @@
+//
+//  GroupRevenuCell.h
+//  WelHair
+//
+//  Created by lu larry on 4/15/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupRevenuCell : UITableViewCell
+
+@end
---
+++ src/ios/App/Views/GroupRevenuCell.m
@@ -0,0 +1,34 @@
+//
+//  GroupRevenuCell.m
+//  WelHair
+//
+//  Created by lu larry on 4/15/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "GroupRevenuCell.h"
+
+@implementation GroupRevenuCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
