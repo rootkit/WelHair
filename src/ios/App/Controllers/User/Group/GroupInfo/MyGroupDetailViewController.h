04ee535e4530f7cbd20dc93c495329dd221c8aab
larrylu <larry@welfony.com>
2014-04-15 23:20:45 +0800

    group info

---
+++ src/ios/App/Controllers/User/Group/GroupInfo/MyGroupDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyGroupDetailViewController.h
+//  WelHair
+//
+//  Created by lu larry on 4/15/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface MyGroupDetailViewController : BaseViewController
+
+@end
