ffa01b99faceb903ab8ca93e86840c00f13f2f72
larrylu <larry@welfony.com>
2014-04-15 22:00:24 +0800

    approval

---
+++ src/ios/App/Views/ApprovalCell.h
@@ -0,0 +1,13 @@
+//
+//  ApprovalCell.h
+//  WelHair
+//
+//  Created by lu larry on 4/15/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ApprovalCell : UITableViewCell
+
+@end
