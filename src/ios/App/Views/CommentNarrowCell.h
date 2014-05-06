5e41228ed60164ce7b783483df676cf3fa9ac845
larrylu <larry@welfony.com>
2014-05-06 21:19:38 +0800

    work detail refine

---
+++ src/ios/App/Views/CommentNarrowCell.h
@@ -0,0 +1,13 @@
+//
+//  CommentNarrowCell.h
+//  WelHair
+//
+//  Created by lu larry on 5/6/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentNarrowCell : UITableViewCell
+
+@end
---
+++ src/ios/App/Views/CommentNarrowCell.m
@@ -0,0 +1,34 @@
+//
+//  CommentNarrowCell.m
+//  WelHair
+//
+//  Created by lu larry on 5/6/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "CommentNarrowCell.h"
+
+@implementation CommentNarrowCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
