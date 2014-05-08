aff8d39fdd81f9aa792febc7477ec8289edd3015
larrylu <larry@welfony.com>
2014-05-08 21:50:16 +0800

    refine group detail

---
+++ src/ios/App/Views/StaffNarrowCell.h
@@ -0,0 +1,13 @@
+//
+//  StaffNarrowCell.h
+//  WelHair
+//
+//  Created by lu larry on 5/8/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StaffNarrowCell : UIView
+
+@end
---
+++ src/ios/App/Views/StaffNarrowCell.m
@@ -0,0 +1,31 @@
+//
+//  StaffNarrowCell.m
+//  WelHair
+//
+//  Created by lu larry on 5/8/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import "StaffNarrowCell.h"
+
+@implementation StaffNarrowCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
