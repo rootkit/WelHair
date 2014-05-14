1b77edb4536f8b3d22c05608ddffe1893277db55
larrylu <larry@welfony.com>
2014-05-14 14:52:45 +0800

    staff add client hair image for appointment

---
+++ src/ios/App/Controllers/User/Staff/UserHairViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserHairViewController.h
+//  WelHair
+//
+//  Created by lu larry on 5/14/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserHairViewController : UIViewController
+
+@end
---
+++ src/ios/App/Controllers/User/UserHairRecordViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserHairRecordViewController.h
+//  WelHair
+//
+//  Created by lu larry on 5/14/14.
+//  Copyright (c) 2014 Welfony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserHairRecordViewController : UIViewController
+
+@end
